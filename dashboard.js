f34ffb16185fe30994a490eec0e988f1e69d70f4
leandrodiaaz <leandrodiaz1119@gmail.com>
2025-07-04 01:30:34 -0400

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,6 @@
+function switchTab(tabId) {
+  document.querySelectorAll('.tab-content').forEach(tab => {
+    tab.classList.remove('active');
+  });
+  document.getElementById(tabId).classList.add('active');
+}
